97c5709cf71c9ff3fd8430474ec483766e9e6a84
Ruslan <rusvolk97i@gmail.com>
2021-02-08 00:21:49 +0300

    layouting 0.3

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+"use strict";
+
 let cog = document.querySelector('.cog-wheel');
+let settingsBar = document.querySelector(".settings");
 cog.addEventListener('click', () =>{
-  
+  console.log(settingsBar);
+  settingsBar.classList.toggle('settings-show');
 });
